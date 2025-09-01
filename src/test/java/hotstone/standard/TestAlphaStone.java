a359e367fba3254c8fa9be06776e7616cc671f06
Bertil Gregor Norup <160720034+BertilNorup@users.noreply.github.com>
2025-09-01 11:52:01 +0200

    d

--- src/test/java/hotstone/standard/TestAlphaStone.java
+++ src/test/java/hotstone/standard/TestAlphaStone.java
@@ -103,35 +103,4 @@ public class TestAlphaStone {
     // assertThat(card.getName(), is(GameConstants.TRES_CARD));
   }
 
-  /** REMOVE ME. Not a test of HotStone, just an example of the
-   matchers that the hamcrest library has... */
-  @Test
-  public void shouldDefinitelyBeRemoved() {
-    // Matching null and not null values
-    // 'is' require an exact match
-    // Hamcrest uses the 'equals()' method
-    String s = null;
-    assertThat(s, is(nullValue()));
-    s = "Ok";
-    assertThat(s, is(notNullValue()));
-    assertThat(s, is("Ok"));
-
-    // If you only validate substrings, use containsString
-    assertThat("This is a dummy test", containsString("dummy"));
-
-    // You can use is on any type
-    int answerToLifeUniverseAndEverything = 42;
-    assertThat(answerToLifeUniverseAndEverything, is(42));
-
-    // Match contents of Lists
-    List<String> l = new ArrayList<String>();
-    l.add("Bimse");
-    l.add("Bumse");
-    // Note - ordering is ignored when matching using hasItems
-    assertThat(l, hasItems(new String[] {"Bumse","Bimse"}));
-
-    // Matchers may be combined, like is-not
-    assertThat(l.get(0), is(not("Bumse")));
-  }
-
 }
